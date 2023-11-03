183a25b53cbd3e4aa524f25113269dafa21faea8
carlos4565mana <carloscal61@gmail.com>
2023-11-03 17:10:29 -0200

    Extract the username from the token

--- src/main/java/com/carlos/security/config/JwtService.java
+++ src/main/java/com/carlos/security/config/JwtService.java
@@ -2,13 +2,18 @@ package com.carlos.security.config;
 
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import org.hibernate.bytecode.internal.bytebuddy.PrivateAccessorException;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 
 @Service
@@ -23,6 +28,17 @@ public class JwtService {
     final Claims claims = extractAllClaims(token);
     return claimsResolver.apply(claims);
   }
+  public String generateToken(Map<String, Object> extraClaims,UserDetails userDetails){
+    return Jwts
+    .builder()
+    .setClaims(extraClaims)
+    .setSubject(userDetails.getUsername())
+    .setIssuedAt(new Date(System.currentTimeMillis()))
+    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+    .compact();
+  }
+  
 
   private Claims extractAllClaims(String token){
     return Jwts
