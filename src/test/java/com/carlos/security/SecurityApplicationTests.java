50c3d0ccd4e536350854968d010b1a5cba59e5dc
carlos4565mana <carloscal61@gmail.com>
2023-11-03 13:33:30 -0200

    Create a new spring boot 3.0 project

---
+++ src/test/java/com/carlos/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
