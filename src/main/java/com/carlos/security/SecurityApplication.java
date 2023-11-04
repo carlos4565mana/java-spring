b4dc82df09991a3a663abc1a8c540e5ac7f49f07
carlos4565mana <carloscal61@gmail.com>
2023-11-04 19:28:12 -0200

    Insert users at application startup

--- src/main/java/com/carlos/security/SecurityApplication.java
+++ src/main/java/com/carlos/security/SecurityApplication.java
@@ -1,13 +1,45 @@
 package com.carlos.security;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.carlos.security.auth.AuthenticationService;
+import com.carlos.security.auth.RegisterRequest;
+
+import static com.carlos.security.user.Role.ADMIN;
+import static com.carlos.security.user.Role.MANAGER;
 
 @SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(
+		AuthenticationService service
+	){
+		return args ->{
+			var admin = RegisterRequest.builder()
+			.firstname("Admin")
+			.lastname("Admin")
+			.email("admin@gmail.com")
+			.password("password")
+			.role(ADMIN)
+			.build();
+			System.out.println("Admin token: "+service.register(admin).getToken());
+
+			var manager = RegisterRequest.builder()
+			.firstname("Admin")
+			.lastname("Admin")
+			.email("manager@gmail.com")
+			.password("password")
+			.role(MANAGER)
+			.build();
+			System.out.println("Admin token: "+service.register(manager).getToken());
+		};
 	}
 
 }
--- src/main/java/com/carlos/security/config/SecurityConfiguration.java
+++ src/main/java/com/carlos/security/config/SecurityConfiguration.java
@@ -1,15 +1,31 @@
 package com.carlos.security.config;
 
-import javax.sound.sampled.AudioFormat;
+import static com.carlos.security.user.Role.ADMIN;
+import static com.carlos.security.user.Role.MANAGER;
+import static com.carlos.security.user.Permission.ADMIN_READ;
+import static com.carlos.security.user.Permission.MANAGER_READ;
+import static com.carlos.security.user.Permission.ADMIN_CREATE;
+import static com.carlos.security.user.Permission.MANAGER_CREATE;
+import static com.carlos.security.user.Permission.ADMIN_UPDATE;
+import static com.carlos.security.user.Permission.MANAGER_UPDATE;
+import static com.carlos.security.user.Permission.ADMIN_DELETE;
+import static com.carlos.security.user.Permission.MANAGER_DELETE;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,9 +44,22 @@ public class SecurityConfiguration {
     .authorizeHttpRequests()
     .requestMatchers("/api/v1/auth/**")
     .permitAll()
+    .requestMatchers("/api/v1/management¹**").hasAnyRole(ADMIN.name(), MANAGER.name())
+    
+    .requestMatchers(HttpMethod.GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+    .requestMatchers(HttpMethod.POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+    .requestMatchers(HttpMethod.PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+    .requestMatchers(HttpMethod.DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+
+    .requestMatchers("/api/v1/admin**").hasRole(ADMIN.name())
+    .requestMatchers(HttpMethod.GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+    .requestMatchers(HttpMethod.POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+    .requestMatchers(HttpMethod.PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+    .requestMatchers(HttpMethod.DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
     .anyRequest()
     .authenticated()
-    .and().sessionManagement()
+    .and()
+    .sessionManagement()
     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     .and()
     .authenticationProvider(authenticationProvider)
