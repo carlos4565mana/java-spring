2da4f0962b510c4b7dc3cdf9d9a234a9b561d7f3
carlos4565mana <carloscal61@gmail.com>
2023-11-06 13:22:26 -0200

    Create the LogoutHandleservice and implement the LogoutHandlerService

--- src/main/java/com/carlos/security/config/SecurityConfiguration.java
+++ src/main/java/com/carlos/security/config/SecurityConfiguration.java
@@ -31,6 +31,7 @@ import static org.springframework.http.HttpMethod.PUT;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +40,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfiguration {
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
+  private final LogoutHandler logoutHandler;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
@@ -73,7 +75,7 @@ public class SecurityConfiguration {
     .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class )
     .logout()
     .logoutUrl("/api/v1/auth/logout")
-    .addLogoutHandler(null)
+    .addLogoutHandler(logoutHandler)
     .logoutSuccessHandler(
             (request, response, authentication) ->
             SecurityContextHolder.clearContext()
